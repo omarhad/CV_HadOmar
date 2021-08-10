79a7df5d836be9aaa7b097e50751d3446659b0aa
omarhad <76268917+omarhad@users.noreply.github.com>
2021-08-10 23:50:27 +0200

    mise a jour de la version final

--- Js/script.js
+++ Js/script.js
@@ -4,3 +4,22 @@ darkMode.addEventListener('change', ()=>{
     document.getElementById('main').classList.toggle('jour');
 }
 )
+const ratio = .8
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+}
+
+const handelIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-visible')
+            observer.unobserve(entry.target)
+        }
+    })
+}
+const observer = new IntersectionObserver(handelIntersect, options);
+document.querySelectorAll('.reveal').forEach(function(r) {
+    observer.observe(r)
+})
